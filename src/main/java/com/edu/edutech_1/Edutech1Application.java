0ea2412d81b695c016758e0285cad01b85d8861a
benjamin ignacio caro gonzalez <be.carog@duocuc.cl>
2025-05-18 01:11:43 -0400

    Inicio de proyecto

---
+++ src/main/java/com/edu/edutech_1/Edutech1Application.java
@@ -0,0 +1,13 @@
+package com.edu.edutech_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edutech1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Edutech1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/edutech_1/Edutech1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.edutech_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Edutech1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
